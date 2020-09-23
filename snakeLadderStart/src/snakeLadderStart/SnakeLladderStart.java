c1ce2eda51b1f4308f754daa59c8581acba528c4
Kushagra5678 <kushu.7feb@hmail.com>
2020-09-23 15:40:03 +0530

    Master commit

---
+++ snakeLadderStart/src/snakeLadderStart/SnakeLladderStart.java
@@ -0,0 +1,10 @@
+package snakeLadderStart;
+
+public class SnakeLladderStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snake And Ladder Game!!!!");
+	}
+
+}
