3ce74973ac469c51d75af3b79dc7a0fe058e815c
Kushagra5678 <kushu.7feb@hmail.com>
2020-09-23 15:43:37 +0530

    UC1 commit

---
+++ snakeLadderUC1/src/snakeLadderUC1/SnkLdrUC1.java
@@ -0,0 +1,11 @@
+package snakeLadderUC1;
+
+public class SnkLdrUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int position=0;
+		System.out.println("Start Position is: "+position);
+	}
+
+}
