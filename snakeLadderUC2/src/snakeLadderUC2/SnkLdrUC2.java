8530a82ce117975f51051e79b6c52c7999fb00b1
Kushagra5678 <kushu.7feb@hmail.com>
2020-09-23 15:46:20 +0530

    UC2 commit

---
+++ snakeLadderUC2/src/snakeLadderUC2/SnkLdrUC2.java
@@ -0,0 +1,12 @@
+package snakeLadderUC2;
+
+public class SnkLdrUC2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int position = 0;
+		int dice_roll=1 + (int)Math.floor(Math.random() * 10)%6;
+		System.out.println("Dice roll is: "+dice_roll);
+	}
+
+}
